8caa4453212fd2e5cc2e125812647e0f0f44018d
matt0852 <matthew.rocketmail@gmail.com>
2021-07-09 10:54:01 +0800

    added scheduler

--- app.js
+++ app.js
@@ -8,40 +8,54 @@ app.use(express.json())
 const axios = require('axios')
 const { response } = require('express')
 
+const schedule = require('node-schedule')
+
 const url = 'https://api.telegram.org/bot' + process.env.TOKEN
+
+var lastChatId
 
 getTextMessage = (req) => {
     if (req.body.message.text && req.body.message.chat.id) {
         let message = {
             text: req.body.message.text,
-            chat_id: req.body.message.chat.id
+            chatId: req.body.message.chat.id
         }
         return message
     }
     else return null
 }
 
-sendMessage = async (chat_id, text) => {
+sendMessage = async (chatId, text) => {
     await axios.get(url + '/sendMessage', {
         params: {
-            chat_id: chat_id,
+            chatId: chatId,
             text: text
         }
     })
 }
 
-sendWelcomeMessage = async (chat_id) => {
+sendWelcomeMessage = async (chatId) => {
     let text = 'Hi! This is the NUS Temperature Reminder Bot. The link https://myaces.nus.edu.sg/htd/htd will be sent at 830am and 1pm daily.'
-    await sendMessage(chat_id, text)
+    await sendMessage(chatId, text)
+}
+
+sendReminderMessage = async (chatId) => {
+    let text = 'Remember to take your temperature! https://myaces.nus.edu.sg/htd/htd'
+    await sendMessage(chatId, text)
 }
 
 manageMessage = async (req, res) => {
     let message = await getTextMessage(req)
     if (message) {
-        if (message.text == '/start') sendWelcomeMessage(message.chat_id)
+        if (message.text == '/start') sendWelcomeMessage(message.chatId)
     }
+    lastChatId = message.chatId
     res.sendStatus(200)
 }
+
+const job = schedule.scheduleJob('0 11 * * *', () => {
+    sendReminderMessage(lastChatId)
+}) 
 
 app.get('/', (req, res) => {
     res.send('NUS Temperature Reminder Bot, webhook added')
